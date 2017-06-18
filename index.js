493c061e4e5c094384956618eca323bbeb470f6a
ravikishorethella <ravikishorethella@gmail.com>
2017-06-18 14:10:20 -0400

    two and three

--- index.js
+++ index.js
@@ -20,4 +20,31 @@
 //    console.log(hours);
 //    console.log(greetings);
 
-//  2
+
+//  2 using a variable to store a number
+
+//    var price;
+//    var quantity;
+//    var total;
+//
+//    price  = 5;
+//    quantity = 10;
+//    total = price * quantity;
+//
+//    var el = document.getElementById("idname");
+//    el.textContent(total);
+
+
+// 3 using a variable to store a string
+
+//    var username;
+//    var message;
+//
+//    username = 'ravi';
+//    message = 'welcome';
+//
+//    var elname = document.getElementById("name");
+//    elname.textContent = username;
+//
+//    var elnote = document.getElementById("note");
+//    elnote.textContent = message;
