bcc33f8e213d6424106af5527e14f7be114c7232
ravikishorethella <ravikishorethella@gmail.com>
2017-06-18 14:03:34 -0400

    one

---
+++ index.js
@@ -0,0 +1,23 @@
+//  1
+
+//    //creating a basic js to find the current time
+//    // to find the date 
+//    var today = new Date();
+//    // to find the current hours
+//    var hours = today.getHours();
+//
+//    var greetings;
+//    if (hours>18){
+//    greetings = 'good evening';
+//    }
+//    else if(hours > 12){
+//    greetings = 'good afternoon';
+//    }
+//    else {
+//    greetings = 'welcome';
+//    }
+//
+//    console.log(hours);
+//    console.log(greetings);
+
+//  2
